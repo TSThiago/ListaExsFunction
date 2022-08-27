2e7d405de1605f08e93be9634cd1d26b5314d0b9
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-27 14:32:53 -0300

    Criação do index e do programa 'exs.js'.

---
+++ exs.js
@@ -0,0 +1,9 @@
+// 1. Faça uma função que recebe um valor inteiro e verifica se o
+// valor é positivo, negativo ou zero. A função deve retornar 1
+// para valores positivos, -1 para valores negativos e 0 para o
+// valor 0.
+// 2. Faça um algoritmo que lê um valor inteiro de reais e calcula o
+// menor valor possível de notas de 100, 50, 10, 5 e 1 que o
+// valor lido pode ser decomposto. Exiba o valor lido e a
+// decomposição. Ex.: 576 // 5 nota(s) de 100 1 nota(s) de 50 2
+// notas(s) de 10 1 nota(s) de 5 1 nota(s) de 1
