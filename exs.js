97cb95578cf75ed15f6063ef5fcd6af63790d885
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-27 14:49:43 -0300

    (Ex 1)  Criação da função que verifica se o número é igual a 0, positivo ou negativo.

--- exs.js
+++ exs.js
@@ -2,6 +2,22 @@
 // valor é positivo, negativo ou zero. A função deve retornar 1
 // para valores positivos, -1 para valores negativos e 0 para o
 // valor 0.
+
+console.log("Ex 1")
+
+function VerificarValor(valor){
+    valor = prompt("Insira o número para verifica-lo")
+    if(valor > 0){
+        console.log("1 - Positivo")
+    }else if(valor == 0 ){
+        console.log("0")
+    }else if(valor < 0){
+        console.log("-1 - Negativo")
+    }
+}
+
+VerificarValor()
+
 // 2. Faça um algoritmo que lê um valor inteiro de reais e calcula o
 // menor valor possível de notas de 100, 50, 10, 5 e 1 que o
 // valor lido pode ser decomposto. Exiba o valor lido e a
